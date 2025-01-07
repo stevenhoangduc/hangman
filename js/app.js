8cdbd03146e3038c4bbc8ee7f7121045235ee993
stevenhoangduc <hoangducsteven@gmail.com>
2025-01-06 22:12:15 -0800

    adding word-list.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
-/*-------------- Constants -------------*/
 
 
-/*---------- Variables (state) ---------*/
+const wordDisplay = document.querySelector(".word-display");
+const keyboardDiv = document.querySelector(".keyboard");
 
+const getRandomWord = () => {
+    // Selecting a random word and hint from the wordList
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word);
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+}
 
-/*----- Cached Element References  -----*/
+const initGame = (button, clickedLetter) => {
+    console.log(button, clickedLetter);
+}
 
-
-/*-------------- Functions -------------*/
-
-
-/*----------- Event Listeners ----------*/
-const keyboardDiv
-
+// Creating keyboard buttons and adding event listeners
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    bottom.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
 }
+
+getRandomWord();
 
---
+++ js/word-list.js
@@ -0,0 +1,98 @@
+const wordlist = [
+    {
+        word: "graduation",
+        hint: "What happens after you finish school or college?"
+    },
+    {
+        word: "mountain",
+        hint: "You climb this to reach the summit."
+    },
+    {
+        word: "father",
+        hint: "A person who is often called 'dad' or 'papa'."
+    },
+    {
+        word: "rainbow",
+        hint: "Appears in the sky after rain and is colorful."
+    },
+    {
+        word: "castle",
+        hint: "A large building where kinds and queens live."
+    },
+    {
+        word: "elephant",
+        hint: "A large animal with a trunk and tusks."
+    },
+    {
+        word: "planet",
+        hint: "A large celestial body, like Earth or Mars"
+    },
+    {
+        word: "school",
+        hint: "A place with lots of trees and wildlife."
+    },
+    {
+        word: "clock",
+        hint: "Tells you the time of day."
+    },
+    {
+        word: "mirror",
+        hint: "Reflects your image when you look at it."
+    },
+    {
+        word: "graduation",
+        hint: "What happens after you finish school or college?"
+    },
+    {
+        word: "jungle",
+        hint: "A dense forest with wild animals."
+    },
+    {
+        word: "library",
+        hint: "A quiet place full of books to read or borrow."
+    },
+    {
+        word: "dragon",
+        hint: "A mythical creature taht breathes fire."
+    },
+    {
+        word: "basket",
+        hint: "Used to carry or hold things, often with a handle."
+    },
+    {
+        word: "island",
+        hint: "Land surrounded by water on all slides."
+    },
+    {
+        word: "Volcano",
+        hint: "A mountain that erupts with lava and smoke."
+    },
+    {
+        word: "puzzle",
+        hint: "A game or problem you solve piece by piece."
+    }, {
+        word: "camera",
+        hint: "Takes pictures or records videos."
+    },
+    {
+        word: "ladder",
+        hint: "Used to climb to higher places."
+    },
+    {
+        word: "winter",
+        hint: "The coldest season of the year"
+    },
+    {
+        word: "pirate",
+        hint: "A person who sails the seas looking for treasure."
+    },
+    {
+        word: "rocket",
+        hint: "A vehicle that travels into space"
+    },
+    {
+        word: "flower",
+        hint: "A colorful plant blooms in spring."
+    },
+    
+]
