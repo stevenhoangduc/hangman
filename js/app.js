28470f286a57c418b5bb0d7554fc273635e5dd98
stevenhoangduc <hoangducsteven@gmail.com>
2025-01-06 16:02:17 -0800

    updating app.js contents

--- js/app.js
+++ js/app.js
@@ -12,3 +12,8 @@
 
 /*----------- Event Listeners ----------*/
 
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+}
+
